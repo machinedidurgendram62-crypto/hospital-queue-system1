59ba3a60381b10d296aef59f5e676b8148d634f7
Durgendra <machinedidurgendram62@.com>
2026-02-25 21:39:01 -0800

    Added dynamic port

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ app.post("/next", (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
